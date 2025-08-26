b67f024d7db3199b326c8024f360a8a82c2ef50d
Simranjeet Singh <simranjeet@s25.digital>
2025-08-26 16:54:34 +0530

    [setup script to run it locally]

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import prettier from "eslint-config-prettier";
+import prettier, { rules } from "eslint-config-prettier";
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -10,5 +10,6 @@ export default tseslint.config(
   {
     ignores: ["dist", "node_modules", "build.js"],
     extends: [prettier],
-  }
+  }, 
+
 );

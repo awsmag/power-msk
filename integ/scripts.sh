18a998c54c2e005b3945129c90159a3a6ff79bec
Simranjeet Singh <simranjeet@s25.digital>
2025-08-27 13:42:10 +0530

    [adding scripts helpful for testing]

--- integ/scripts.sh
+++ integ/scripts.sh
@@ -1,1 +1,8 @@
+# create topic
 podman exec -it kafka kafka-topics.sh --bootstrap-server localhost:9092 --create  --topic test
+
+# produce message
+podman exec -it kafka \
+  kafka-console-producer.sh \
+    --bootstrap-server localhost:9092 \
+    --topic test
